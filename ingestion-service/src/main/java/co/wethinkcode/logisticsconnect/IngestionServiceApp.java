10d3809a33bd93e5f8b603f784ef6643dc2a453c
Chantel Muthaphuli <chantelmuthaphuli@gmail.com>
2026-09-15 12:58:19 +0200

    feat: starte code for ingestion service

--- ingestion-service/src/main/java/co/wethinkcode/logisticsconnect/IngestionServiceApp.java
+++ ingestion-service/src/main/java/co/wethinkcode/logisticsconnect/IngestionServiceApp.java
@@ -1,13 +1,37 @@
 package co.wethinkcode.logisticsconnect;
 
 import io.javalin.Javalin;
+import com.opencsv.*;
+import java.io.FileReader;
+import java.util.*;
+import java.nio.charset.StandardCharsets;
 
 public class IngestionServiceApp {
+    private static List<Map<String,Object>> cleanCsv(String path){
+        List<String[]> rawRecords;
+
+        try(InputStream in = IngestionService.class.getResourceAsStream(resourcePath));
+
+        CVSReader csvReader = new CSVReader(new InputStreamReader(in,StandardCharsets.UFT_8)){
+            List<String[]> allRows = csvReader.readAll();
+
+            rawRecords = allRows.subList(1,allRows.size());
+        }
+        // fix id's
+
+        // fix provinces
+
+        //fix centers
+
+        //fix active boolean
+
+    }
 
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7050);
 
         app.get("/health", ctx -> ctx.result("OK"));
+        app.get("/hubs"), ctx -> ctx.json(hubs)
 
         // TODO: read and clean src/main/resources/hubs-global.csv (hubs, sorting centers, regional districts data —
         // trim whitespace, fix casing, normalize dates/booleans) and expose the
