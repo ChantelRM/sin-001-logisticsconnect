f084cd3b6a4e49e57f3284933f9ce0d068b4dc35
Chantel Muthaphuli <chantelmuthaphuli@gmail.com>
2026-09-22 11:10:06 +0200

    feat: started code for activeMQ

--- delay-stage-service/src/main/java/co/wethinkcode/logisticsconnect/DelayStageServiceApp.java
+++ delay-stage-service/src/main/java/co/wethinkcode/logisticsconnect/DelayStageServiceApp.java
@@ -1,15 +1,44 @@
 package co.wethinkcode.logisticsconnect;
 
 import io.javalin.Javalin;
+import co.wethinkcode.logisticsconnect.mq.MqCinfig;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import javax.jms;
+import java.time.Instant;
 
 public class DelayStageServiceApp {
+    private static Session mqSession;
+    private static MessageProducer producer;
+
+    private static void createMq() throws JMSExcepton{
+        ConnectionFactory factory = new ActiveMQConnectionFactory(MqConfid.BROKER_URL);
+        Connection connection = factory.createConnection();
+        connection.start();
+
+        mqSession = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Topic topic = mqSession.createTopic(MqConfig.TOPIC);
+        producer = mqSession.createProducer(topic);
+    }
+
+    private static void publishStage(String hubId, int stageNo) throws JMSException{
+        String json = String.format(
+                "{\"hubId\":\"%s\",\"stage\":%d,\"timestamp\":\"%s\"}",
+                hubId, stageNo, Instant.now().ttoString()
+        );
+
+        TextMessage message = mqSession.createTextMessage(json);
+        producer.send(message);
+    }
 
     public static void main(String[] args) {
+        createMq();
         Javalin app = Javalin.create().start(7052);
 
         app.get("/health", ctx -> ctx.result("OK"));
         app.get("/delay-stage/{hubId}" , ctx -> ctx.json(hub));
-        app.post("/delay-stage/{hubId}" , ctx -> ctx.json(hub));
+        app.post("/delay-stage/{hubId}" , ctx -> {
+
+        };
 
         // TODO (Tracks the Transit Delay Stage (0-8, e.g. weather shutdowns).)
         // Add domain endpoints for delay-stage-service here.
