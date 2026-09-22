17cf3a6968b91f02c882caf57a88482806d63cc4
Chantel Muthaphuli <chantelmuthaphuli@gmail.com>
2026-09-22 12:42:03 +0200

    feat: transit mq subscribe starter code

--- transit-service/src/main/java/co/wethinkcode/logisticsconnect/TransitServiceApp.java
+++ transit-service/src/main/java/co/wethinkcode/logisticsconnect/TransitServiceApp.java
@@ -1,8 +1,45 @@
 package co.wethinkcode.logisticsconnect;
 
 import io.javalin.Javalin;
+import co.wethinkcode.logisticsconnect.mq.MqConfig;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import javax.jms.*;
+import java.util.*;
 
 public class TransitServiceApp {
+    private static final Map<String, Integer> updatedHubStages = new ConcurrentHashMap<>();
+
+    private static void subscribe() throws JMSException{
+        ConnectionFactory fcatory = new ActiveMQConnectionFactory(MqConfig.BROKER_URL);
+        Connection connection = factory.createConnection();
+        connection.start();
+
+        Session mqSession = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Topic topic = mqSession.createTopic(MqConfig.TOPIC);
+
+        MessageConsumer consumer = session.createConsumer(topic);
+        consumer.setMessageListener(message ->
+                {
+                    try{
+                        if(message instanceof TextMessage textMessage){
+                            String json = textMessage.getText();
+
+                            ObjectMapper mapper = new ObjectMapper();
+                            Map<String, Object> parsed = mapper.readValue(json, new TypeRefernce<Map<String,Object>>() {});
+
+                            String id= (String) parsed.get("hubId");
+                            Integer stage =(Integer) parsed.get("stage");
+
+                            updatedHubStages.put(id,stage);
+
+                        }
+                    } catch(JMSException e) {
+                        e.printStackTrace();
+                    }
+                }
+         );
+
+    }
 
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7053);
