18356f2d3e296c3ec6e53e5a063a1e86a2aab034
Chantel Muthaphuli <chantelmuthaphuli@gmail.com>
2026-09-24 12:53:32 +0200

    fix: alert bot

--- alertbot/src/main/java/co/wethinkcode/logisticsconnect/AlertBotApp.java
+++ alertbot/src/main/java/co/wethinkcode/logisticsconnect/AlertBotApp.java
@@ -54,10 +54,5 @@ public class AlertBotApp {
 
         app.get("/health", ctx -> ctx.result("OK"));
 
-        // TODO (Posts proactive delay notifications to public transit social media pages (simulated).)
-        // Mechanism: Outbound webhook, simulated social post
     }
 }
-
-// MQ TODO (stretch goal): subscribes to ActiveMQ topic MqConfig.TOPIC at MqConfig.BROKER_URL (see co.wethinkcode.logisticsconnect.mq.MqConfig)
-// alert bot stimulation:
